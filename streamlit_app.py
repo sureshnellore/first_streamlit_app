6409e328bd252ab4a990a57c0c6c668a14fc227d
sureshnellore <sureshnellore.cme@gmail.com>
2023-11-22 14:25:59 +0530

    latest changes indexing2

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,18 +12,18 @@ streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
-my_fruit_list = my_fruit_list.set_index('Fruit')
+#my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-streamlit.dataframe(my_fruit_list)
+#streamlit.dataframe(my_fruit_list)
 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 
-#streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
