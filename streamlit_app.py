d8ed41f8a3c22e3874fcaff1a13b349fc6a5ee42
sureshnellore <sureshnellore.cme@gmail.com>
2023-11-24 11:40:28 +0530

    added snowflake connector

--- streamlit_app.py
+++ streamlit_app.py
@@ -17,7 +17,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 #my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-#streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(my_fruit_list)
 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
@@ -25,16 +25,31 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 
 #streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruits_to_show)
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-streamlit.text(fruityvice_response)
+#streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
+#import requests
+#fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
+#streamlit.text(fruityvice_response)
+
+#streamlit.header("Fruityvice Fruit Advice!")
+
+
+import snowflake.connector
+
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
+
+
+
+
